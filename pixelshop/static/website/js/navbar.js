8a7a50a9258b6e3e0e4eb28e8c7596213837cae8
Sirothe <58854544+Sirothe@users.noreply.github.com>
2022-01-08 22:11:21 +0100

    Cards update
    Added cards
    Changed navbar.js to move content whenever opened

--- pixelshop/static/website/js/navbar.js
+++ pixelshop/static/website/js/navbar.js
@@ -1,6 +1,10 @@
-let btn = document.querySelector("#btn");
-let sidebar = document.querySelector(".sidebar");
+$('document').ready(function(){
 
-btn.onclick = function() {
-    sidebar.classList.toggle("active");
-}
+    let btn = document.querySelector("#btn");
+    let sidebar = document.querySelector(".sidebar");
+    let container = document.querySelector(".container")
+
+    btn.onclick = function() {
+        sidebar.classList.toggle("active");
+        container.classList.toggle("activenav");
+    }})
