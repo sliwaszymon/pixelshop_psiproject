07233800da75045a72fa679485c903e4a1246407
Sirothe <58854544+Sirothe@users.noreply.github.com>
2022-01-09 01:17:09 +0100

    View update
    Changed containers to fluid containers so whenever sidebar is opened content is moved along with it

--- pixelshop/static/website/js/navbar.js
+++ pixelshop/static/website/js/navbar.js
@@ -2,9 +2,19 @@ $('document').ready(function(){
 
     let btn = document.querySelector("#btn");
     let sidebar = document.querySelector(".sidebar");
-    let container = document.querySelector(".container")
+    let content_shop = document.querySelector(".content-shop");
+    let content_main = document.querySelector(".content-main");
 
     btn.onclick = function() {
         sidebar.classList.toggle("active");
-        container.classList.toggle("activenav");
+        try{
+            content_shop.classList.toggle("activenav");
+        }catch{
+            null;
+        }
+        try{
+            content_main.classList.toggle("activenav");
+        }catch{
+            null;
+        }
     }})
