d3653086fa3f3c6793c05300db0246c7fe7f78a4
szymon242820 <58854456+szymon242820@users.noreply.github.com>
2022-01-15 22:52:50 +0100

    images repaired
    added new simple tag that changes direction of slashes

--- pixelshop/pixelshop/templatetags/simple_tags.py
+++ pixelshop/pixelshop/templatetags/simple_tags.py
@@ -7,3 +7,9 @@ def get_pk(value):
     value = value.split('/')
     value = value[::-1]
     return int(value[1])
+
+@register.filter("redirect_slashes")
+def redirect_slashes(value):
+    value = value.split('\\')
+    value = '/'.join(value)
+    return f"/{value}"
