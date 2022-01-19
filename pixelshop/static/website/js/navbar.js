185f68025c2f5f01d04357ff859a9a6225643602
Sirothe <58854544+Sirothe@users.noreply.github.com>
2022-01-19 01:27:47 +0100

    About Us page
    Added images of devs
    Created cards to hold information about devs
    Added to navbar function moving content when opened

--- pixelshop/static/website/js/navbar.js
+++ pixelshop/static/website/js/navbar.js
@@ -4,6 +4,7 @@ $('document').ready(function(){
     let sidebar = document.querySelector(".sidebar");
     let content_shop = document.querySelector(".content-shop");
     let content_main = document.querySelector(".content-main");
+    let content_about_us = document.querySelector(".content-about-us");
 
     btn.onclick = function() {
         sidebar.classList.toggle("active");
@@ -14,6 +15,11 @@ $('document').ready(function(){
         }
         try{
             content_main.classList.toggle("activenav");
+        }catch{
+            null;
+        }
+        try{
+            content_about_us.classList.toggle("activenav");
         }catch{
             null;
         }
